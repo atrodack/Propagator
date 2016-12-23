ffb2e0ee0da8ded8f3e597f2568d6daf275830e0
atrodack <atrodack@email.arizona.edu>
2016-12-23 15:46:46 -0700

    Adding in Propagation Methods and other utilities....not yet complete

--- Propagator/@OptSys/OptSys.m
+++ Propagator/@OptSys/OptSys.m
@@ -16,14 +16,33 @@ classdef OptSys < matlab.mixin.Copyable
     end % of public properties
     
     properties(GetAccess = 'public', SetAccess = 'protected')
+        
+        % System Parameters
         numElements_ = 0; % number of elements in system
         f_number_; % F/# of system
-        ELEMENTS_; % cell array of element cards
-        apertureStop;
+        conjugations_ = zeros(1,1); % list of z locations
+        pscale_; % size of pixels in meters
+        gridsize_; %
+        lambda0_; % central wavelength
+        lambda_array_; % vector of lambdas to use
+        apertureStop; % probably not needed
         
-        conjugations_ = zeros(1,1);
-        pscale;
-        lambda_array_;
+        % List of Elements
+        ELEMENTS_; % cell array of element cards
+        % Each element is of type OptElement
+        % This includes the information:
+        % 1) Element type
+        % 2) Material (index of refraction)
+        % 3) Element Position
+        % 4) z-sag in meters
+        % 5) Type of propagation to use (could change)
+           
+        % Wavefront
+        WFamp;
+        WFphase;
+        WFreal;
+        WFimag;
+        WF;
         
         % GPU properties
         useGPU; % use a GPU flag
@@ -45,10 +64,15 @@ classdef OptSys < matlab.mixin.Copyable
         
         %% Constructor
         
-        function OS = OptSys()
-            % OS = OptSys()
+        function OS = OptSys(sz)
+            % OS = OptSys(sz)
+            % Constructs OptSys object. Initializes WFCA to size sz
             
-            OS.initOptSys;
+            if nargin < 1
+                sz = 1024;
+            end
+            
+            OS.initOptSys(sz);
             
             
         end % of contructor
@@ -56,16 +80,18 @@ classdef OptSys < matlab.mixin.Copyable
         
         %% Set Properties
         
-        function OS = initOptSys(OS)
-            %OS = initializeOptSys(OS)
+        function OS = initOptSys(OS,sz)
+            %OS = initializeOptSys(OS,sz)
             
             OS.ELEMENTS_ = cell(OS.numElements_,1);
             OS.nGPUs = gpuDeviceCount;
             if OS.nGPUs > 0
                 OS.initGPU;
             end
+            OS.setGridsize(sz);
             
         end % of initOptSys
+        
         
         function OS = addSequentialElement(OS,elem)
             % OS = addSequentialElement(OS,elem)
@@ -85,6 +111,7 @@ classdef OptSys < matlab.mixin.Copyable
                 error('I do not understand anything but OptElement objects');
             end
         end % of addSequentialElement
+        
         
         function OS = addElement(OS,elem,order_num)
             % OS = addElement(OS,elem,order_num)
@@ -153,10 +180,28 @@ classdef OptSys < matlab.mixin.Copyable
             else
                 fprintf('Element %s has not been removed\n',selected_elem.name);
             end
-                        
-            
         end % of removeElement
                 
+        
+        function OS = setPscale(OS,val)
+            % OS = setPscale(OS,val)
+            OS.pscale_ = val;
+        end % of setPscale
+        
+        
+        function OS = setLambdaarray(OS,lambdalist)
+            % OS = setLambdaarray(OS,lambdalist)
+            
+            OS.lambda_array_ = lambdalist;
+        end % of setLambdaarray
+        
+        
+        function OS = setCentralWavelength(OS,lambda)
+            % OS = setCentralWavelength(OS,lambda)
+            
+            OS.lambda0_ = lambda;
+        end % of setCentralWavelength
+        
         
         function conjugations = setConjugations(OS)
             % OS = setConjugations(OS)
@@ -170,16 +215,271 @@ classdef OptSys < matlab.mixin.Copyable
             
         end % of setConjugations
         
+        
         function OS = getElementFNum(OS,element_num)
             % OS = getElementFNum(OS,element_num)
             % sets the f/# of the system to that of an element
             
             OS.f_number_ = OS.ELEMENTS_{element_num}.getFNumber;
         end % of getElementFNum
+        
+        
+        function OS = setGridsize(OS,sz)
+            % gridsize = setGridsize(OS,sz)
             
+            if nargin < 2
+                sz = size(OS.ELEMENTS_{1}.zsag_,1);
+            end
+            OS.gridsize_ = sz;
+        end % of setGridsize
+           
+        
         %% Propagation Methods
         
+        function [numLambdas,propdist] = initPropagation(OS,starting_elem, ending_elem)
+            % [numLambdas,propdist] = initPropagation(OS,starting_elem, ending_elem)
+            % Runs checks on OS object to ensure properties needed to
+            % propagate through system are set.
+            %
+            % Returns number of lambdas and list of propagation distances
+            % for use in PropagateSystem method
+            
+            OK = false(3,1);
+            % Check that given Elements exist
+            if OS.numElements_ >= starting_elem
+                if OS.numElements_ <= ending_elem
+                    OK(1,1) = true;
+                else
+                    fprintf('\n\n');
+                    warning('Element %d does not exist',ending_elem);
+                end
+            else
+                fprintf('\n\n');
+                warning('Element %d does not exist',starting_elem);
+            end
+            
+            % Check that pscale_ is set
+            if isempty(OS.pscale_) == 0
+                OK(2,1) = true;
+            else
+                fprintf('\n\n');
+                warning('Pixel Scale is not set');
+            end
+            
+            % Check Wavelength
+            if isempty(OS.lambda_array_) == 0
+                numLambdas = length(OS.lambda_array_);
+                OK(3,1) = true;
+            else
+                if isempty(OS.lambda0_) == 0
+                    numLambdas = 1;
+                    OK(3,1) = true;
+                else
+                    fprintf('\n\n');
+                    warning('Central Wavelength and Lambda_array are empty');
+                end
+            end
+            
+            % Error out if a Check fails
+            if prod(OK) == 0
+                fprintf('\n\n');
+                error('Check failed. See above warnings for help');
+            end
+            
+            
+            % Get the z_position list
+            if isempty(OS.conjugations_) == 0
+                if length(OS.conjugations_) == OS.numElements_
+                    z_list = OS.conjugations_; % copy if already cached
+                else
+                    z_list = OS.setConjugations; % cache the values
+                end
+            end
+            propdist = zeros(length(z_list)-1,1);
+            for ii = 1:length(z_list)-1
+                propdist(ii) = z_list(ii+1) - z_list(ii);
+            end
+            
+        end % initPropagation
+         
         
+        function planewave = planewave(OS,amplitude,theta)
+            % OS = planewave(OS,amplitude,theta)
+            % Initializes a planewave as the stored wavefront
+            %
+            % TO DO: add off-axis planewave functionality
+            
+            % If no input, just use ones
+            if nargin < 2
+                amplitude = complex(1);
+            end
+            
+            % Theta is the off axis angle of the planewave (not supported
+            % yet)
+            if nargin < 3
+                on_axis = true;
+            else
+                on_axis = false;
+            end
+            
+            if on_axis
+                OS.WFamp = amplitude .* ones(OS.gridsize_);
+                OS.WFphase = zeros(OS.gridsize_);
+                OS.AmpPhase2WF;
+                planewave = OS.WF;
+            else  % off-axis
+                error('Not supported yet'); 
+            end
+        end % of planewave
+        
+        
+        function OS = AmpPhase2WF(OS)
+            % OS = AmpPhase2WF(OS)
+            % Sets the WF as the combination of the amplitude and phase
+            % components
+            
+            OS.WF = OS.WFamp .* exp(-1i * OS.WFphase);
+        end % of AmpPhase2WF
+        
+        
+        function [WFreal,WFimag] = ReImWF(OS,WFin)
+            % OS = ReImWF(OS,WFin)
+            % Finds the real and imaginary components of a wavefront. If no
+            % wavefront is given, acts on wavefront stored in OptSys
+            % object, and stores results in object
+            
+            if nargin < 2
+                if isempty(OS.WF) == 0
+                    OS.WFreal = real(OS.WF);
+                    OS.WFimag = imag(OS.WF);
+                else
+                    error('No wavefront stored in Object');
+                end
+            else
+                WFreal = real(WFin);
+                WFimag = imag(WFin);
+            end
+        end % of ReImWF
+        
+        
+        function [WFreal,WFimag] = WFAmpPhase2ReIm(OS,WFamp,WFphase)
+            % [WFreal,WFimag] = WFAmpPhase2ReIm(OS,WFamp,WFphase)
+            % sets the real and imaginary parts of the wavefront from
+            % amplitude and phase components. If no amplitude and phase
+            % components are given, act on stored ones, and store resutls
+            
+            if nargin < 2
+                sz = size(OS.WF);
+                for ii = 1:sz(1)
+                    for jj = 1:sz(2)
+                        amp = OS.WFamp(ii,jj);
+                        phase = OS.WFphase(ii,jj);
+                        OS.WFreal(ii,jj) = amp*cos(phase);
+                        OS.WFimag(ii,jj) = amp*sin(phase);
+                    end
+                end
+            elseif nargin == 3
+                sz = size(WFamp);
+                WFreal = zeros(sz(1),sz(2));
+                WFimag = zeros(sz(1),sz(2));
+                for ii = 1:sz(1)
+                    for jj = 1:sz(2)
+                        amp = WFamp(ii,jj);
+                        phase = WFphase(ii,jj);
+                        WFreal(ii,jj) = amp*cos(phase);
+                        WFimag(ii,jj) = amp*sin(phase);
+                    end
+                end
+            end
+        end % of WF2ReIm
+        
+        
+        function [WFamp,WFphase] = WFReIm2AmpPhase(OS,WFreal,WFimag)
+            % [WFamp,WFphase] = WFReIm2AmpPhase(OS,WFreal,WFimag)
+            % Converts real and imaginary parts of WF to amplitude and
+            % phase.
+            
+            if nargin < 2
+                sz = size(OS.WF);
+                for ii = 1:sz(1)
+                    for jj = 1:sz(2)
+                        re = OS.WFreal(ii,jj);
+                        im = OS.WFimag(ii,jj);
+                        amp = sqrt(re*re + im*im);
+                        phase = atan2(im,re);
+                        OS.WFamp(ii,jj) = amp;
+                        OS.WFphase(ii,jj) = phase;
+                    end
+                end
+            elseif nargin == 3
+                sz = size(WFreal);
+                WFamp = zeros(sz(1),sz(2));
+                WFphase = zeros(sz(1),sz(2));
+                for ii = 1:sz(1)
+                    for jj = 1:sz(2)
+                        re = WFreal(ii,jj);
+                        im = WFimag(ii,jj);
+                        amp = sqrt(re*re + im*im);
+                        phase = atan2(im,re);
+                        WFamp(ii,jj) = amp;
+                        WFphase(ii,jj) = phase;
+                    end
+                end
+            end
+                
+        end % of WFReIm2AmpPhase
+        
+        
+        function OS = PropagateSystem(OS, WFin, starting_elem, ending_elem)
+            % OS = PropagateSystem(OS, WFin, starting_elem, ending_elem)
+            % Propagates WFin through the elements [starting_elem,
+            % starting_elem + 1, starting_elem +2, ..., ending_elem].
+            %
+            % WFin should be a complex amplitude (amp .* exp(-i*phase))
+            %
+            % TO DO:
+            % Add in applying of elements
+            % Convert from WF re/im to amp/phase
+            
+            [numLambdas, propdist] = OS.initPropagation(starting_elem, ending_elem);
+            
+            if numLambdas == 1
+                lambda = OS.lambda0_;
+            else
+                lambda = OS.lambda_array_;
+            end
+            
+            WFout = zeros(size(WFin,1),size(WFin,2),numLambdas);
+            
+            for ii = starting_elem:ending_elem
+                if ii ~= ending_elem
+                for jj = 1:numLambdas
+                    WFout(:,:,jj) = OptSys.FresnelPropagateWF(WFin(:,:,jj),propdist(ii),OS.pscale_,lambda(jj));
+                    
+                    if OS.verbose == 1
+                        figure(9999)
+                        imagesc(real(WFout(:,:,jj)));
+                        axis square;
+                        axis xy;
+                        colorbar;
+                        title(sprintf('WF00%d, lambda = %g',ii,lambda(jj)));
+                        drawnow;
+                    end
+                end
+                else % last element
+                    if OS.ELEMENTS_{ii}.isFocal == true
+                        fprintf('Computing PSF using FFT of WF\n');
+                        fprintf('Not currently supported...\n');
+                    else
+                        fprintf('At last element\n');
+                    end
+                end
+            end
+            
+            WFin = WFout;
+                    
+            
+        end % of PropagateSystem
         
         
         %% GPU Methods
@@ -241,6 +541,46 @@ classdef OptSys < matlab.mixin.Copyable
     
     %% Static Methods
     methods(Static = true)
+        
+        function WFout = FresnelPropagateWF(WFin, propdist, pscale, lambda)
+            % WFout = FresnelPropagateWF(WFin, propdist, pscale, lambda)
+            % Fresnel Propagates a wavefront
+        
+            % Initializations
+            naxes0 = size(WFin,1);
+            naxes1 = size(WFin,2);
+            n0h = naxes0 / 2;
+            n1h = naxes1 / 2;
+            
+            coeff = pi*propdist*lambda / (pscale * naxes0) / (pscale * naxes1);
+            
+            % Propagate
+            tmp = fftshift(fft2(fftshift(WFin))) * pscale * pscale;
+            tmpre = real(tmp);
+            tmpim = imag(tmp);
+            for jj = 0:naxes1-1
+                jj1 = naxes0*jj;
+                jj2 = (jj - n1h)^2;
+                for ii = 0:naxes0-1
+                    ii1 = jj1+ii;
+                    ii2 = ii-n0h;
+                    sqdist = ii2^2 + jj2;
+                    angle = -coeff*sqdist;
+                    re = tmpre(ii1+1);
+                    im = tmpim(ii1+1);
+                    tmpre(ii1+1) = re*cos(angle) - im*sin(angle);
+                    tmpim(ii1+1) = re*sin(angle) + im*cos(angle);
+                    
+%                     figure(1);
+%                     imagesc(tmpre);
+%                     title(sprintf('jj = %d, ii = %d',jj,ii));
+%                     drawnow;
+                end
+            end
+            tmp = tmpre + 1i*tmpim;
+            WFout = ifftshift(ifft2(ifftshift(tmp))) * (1/pscale) * (1/pscale);
+            
+        end % of FresnelPropagateWF
         
         
     end % of static methods
