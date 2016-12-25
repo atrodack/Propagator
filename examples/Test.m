395e49882ee250d0c17726ecb7bb609893862f0f
atrodack <atrodack@email.arizona.edu>
2016-12-24 23:33:10 -0700

    Update Test script to test propagation through a pupil, a PIAA mirror pair, and then take the field to a focal plane

--- examples/Test.m
+++ examples/Test.m
@@ -4,7 +4,7 @@ clear all; clc;
 
 %% Material Set up
 
-lambda = linspace(550e-9 - 550e-9 /5, 550e-9 + 550e-9 / 5, 25); 
+lambda = linspace(550e-9 - 550e-9 /50, 550e-9 + 550e-9 / 50, 25); 
 OM = OptMaterial(2,lambda);
 phasefac = OM.ComputePhaseFactor(550e-9);
 
@@ -20,34 +20,45 @@ props{4,1} = 273.2e-3; % focal length
 props{5,1} = 100; % z position
 props{6,1} = 3.4; % diameter
 props{7,1} = A; % zsag
-props{8,1} = 0; % propagation type
+props{8,1} = 0; % isFocal
 E1 = OptElement(props);
 
 
 B = 'piaam0z.fits';
 props2 = cell(8,1);
-props2{1,1} = 'Mirror'; % name
-props2{2,1} = 1; % type
+props2{1,1} = 'PIAA Mirror 1'; % name
+props2{2,1} = 4; % type
 props2{3,1} = OM;
 props2{4,1} = 10; % focal length
 props2{5,1} = 1.2; % z position
 props2{6,1} = 25.4e-3; % diameter
 props2{7,1} = B; % zsag
-props2{8,1} = 0; % propagation type
+props2{8,1} = 0; % isFocal
 E2 = OptElement(props2);
 
-C = ones(1024);
+C = 'piaam1z.fits';
 props3 = cell(8,1);
-props3{1,1} = 'Mirror'; % name
-props3{2,1} = 1; % type
+props3{1,1} = 'PIAA Mirror 2'; % name
+props3{2,1} = 4; % type
 props3{3,1} = OM;
 props3{4,1} = 10; % focal length
-props3{5,1} = 15; % z position
+props3{5,1} = -1.102609; % z position
 props3{6,1} = 25.4e-3; % diameter
-props3{7,1} = B; % zsag
-props3{8,1} = 1; % propagation type
+props3{7,1} = C; % zsag
+props3{8,1} = 0; % isFocal
 E3 = OptElement(props3);
 
+D = ones(1024);
+props4 = cell(8,1);
+props4{1,1} = 'Detector'; % name
+props4{2,1} = 7; % type
+props4{3,1} = OM;
+props4{4,1} = 10; % focal length
+props4{5,1} = -1.102609; % z position
+props4{6,1} = 25.4e-3; % diameter
+props4{7,1} = D; % zsag
+props4{8,1} = 1; % isFocal
+E4 = OptElement(props4);
 
 %% Optical System
 
@@ -61,13 +72,14 @@ OS.setPscale(0.00011);
 OS.addSequentialElement(E1);
 OS.addSequentialElement(E2);
 OS.addSequentialElement(E3);
+OS.addSequentialElement(E4);
 
-field1 = OS.planewave(OS.ELEMENTS_{1}.zsag_);
+field1 = OS.planewave(1);
 field = ones(size(field1,1),size(field1,2),length(lambda));
 for ii = 1:length(lambda)
     field(:,:,ii) = field1;
 end;
 
 % Not yet working fully
-OS.PropagateSystem(field, 1, 3);
+OS.PropagateSystem(field, 1, 4);
 
