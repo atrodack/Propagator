5d407e2a0ebef87b44fcb82588d7dce3d3d9b164
atrodack <atrodack@email.arizona.edu>
2016-12-29 21:05:58 -0700

    Updated to handle matrices with 3rd Dimmension > 1

--- Propagator/Utilities/WFReIm2AmpPhase2.m
+++ Propagator/Utilities/WFReIm2AmpPhase2.m
@@ -7,22 +7,39 @@ if nargin < 2
     if isa(OS,'OptSys') == 0
         error('Argument must be an OptSys object');
     end
-
-    amp = sqrt(OS.WFreal.*OS.WFreal + OS.WFimag.*OS.WFimag);
-    phase = atan2(OS.WFimag,OS.WFreal);
-    OS.WFamp = amp;
-    OS.WFphase = phase;
+    
+    numLambdas = size(OS.WFreal,3);
+    for jj = 1:numLambdas
+        amp = sqrt(OS.WFreal(:,:,jj).*OS.WFreal(:,:,jj) + OS.WFimag(:,:,jj).*OS.WFimag(:,:,jj));
+        phase = atan2(OS.WFimag(:,:,jj),OS.WFreal(:,:,jj));
+        OS.WFamp(:,:,jj) = amp;
+        OS.WFphase(:,:,jj) = phase;
+    end
     
 elseif nargin == 2 % Assume user isn't using an OptSys object, save input OS as WFamp, and WFamp as WFphase
     WFimag = WFreal;
-    WFreal = OS;
+    WFreal = OS;    
+    numLambdas = size(WFreal,3);
     
-    WFamp = sqrt(WFreal.*WFreal + WFimag.*WFimag);
-    WFphase = atan2(WFimag,WFreal);
+    WFamp = zeros(size(WFreal,1),size(WFreal,2),numLambdas);
+    WFphase = WFamp;
+
+    for jj = 1:numLambdas
+        WFamp(:,:,jj) = sqrt(WFreal(:,:,jj).*WFreal(:,:,jj) + WFimag(:,:,jj).*WFimag(:,:,jj));
+        WFphase(:,:,jj) = atan2(WFimag(:,:,jj),WFreal(:,:,jj));
+    end
     
 elseif nargin == 3
-    WFamp = sqrt(WFreal.*WFreal + WFimag.*WFimag);
-    WFphase = atan2(WFimag,WFreal);
+    numLambdas = size(WFreal,3);
+    
+    WFamp = zeros(size(WFreal,1),size(WFreal,2),numLambdas);
+    WFphase = WFamp;
+
+    for jj = 1:numLambdas
+        WFamp(:,:,jj) = sqrt(WFreal(:,:,jj).*WFreal(:,:,jj) + WFimag(:,:,jj).*WFimag(:,:,jj));
+        WFphase(:,:,jj) = atan2(WFimag(:,:,jj),WFreal(:,:,jj));
+    end
+    
 end
 
 end % of WFReIm2AmpPhase2
