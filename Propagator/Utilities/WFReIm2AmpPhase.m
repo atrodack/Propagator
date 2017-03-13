e642d2374195d8f7f56fb5a9fbcff83e23cc79a1
atrodack <atrodack@email.arizona.edu>
2017-03-12 18:53:43 -0700

    Removing dependence on no longer existing OptSys properties

--- Propagator/Utilities/WFReIm2AmpPhase.m
+++ Propagator/Utilities/WFReIm2AmpPhase.m
@@ -3,22 +3,8 @@ function [WFamp,WFphase] = WFReIm2AmpPhase(OS,WFreal,WFimag)
 % Converts real and imaginary parts of WF to amplitude and
 % phase.
 
-if nargin < 2
-    if isa(OS,'OptSys') == 0
-        error('Argument must be an OptSys object');
-    end
-    sz = size(OS.WF);
-    for ii = 1:sz(1)
-        for jj = 1:sz(2)
-            re = OS.WFreal(ii,jj);
-            im = OS.WFimag(ii,jj);
-            amp = sqrt(re*re + im*im);
-            phase = atan2(im,re);
-            OS.WFamp(ii,jj) = amp;
-            OS.WFphase(ii,jj) = phase;
-        end
-    end
-elseif nargin == 2 % Assume user isn't using an OptSys object, save input OS as WFamp, and WFamp as WFphase
+
+if nargin == 2 % Assume user isn't using an OptSys object, save input OS as WFamp, and WFamp as WFphase
     WFimag = WFreal;
     WFreal = OS;
     sz = size(WFreal);
@@ -48,6 +34,10 @@ elseif nargin == 3
             WFphase(ii,jj) = phase;
         end
     end
+    
+    % set OS
+    OS.WFamp = WFamp;
+    OS.WFphase = WFphase;
 end
 
 end % of WFReIm2AmpPhase
--- Propagator/Utilities/WFReIm2AmpPhase2.m
+++ Propagator/Utilities/WFReIm2AmpPhase2.m
@@ -3,20 +3,8 @@ function [WFamp,WFphase] = WFReIm2AmpPhase2(OS,WFreal,WFimag)
 % Converts real and imaginary parts of WF to amplitude and
 % phase.
 
-if nargin < 2
-    if isa(OS,'OptSys') == 0
-        error('Argument must be an OptSys object');
-    end
-    
-    numLambdas = size(OS.WFreal,3);
-    for jj = 1:numLambdas
-        amp = sqrt(OS.WFreal(:,:,jj).*OS.WFreal(:,:,jj) + OS.WFimag(:,:,jj).*OS.WFimag(:,:,jj));
-        phase = atan2(OS.WFimag(:,:,jj),OS.WFreal(:,:,jj));
-        OS.WFamp(:,:,jj) = amp;
-        OS.WFphase(:,:,jj) = phase;
-    end
-    
-elseif nargin == 2 % Assume user isn't using an OptSys object, save input OS as WFamp, and WFamp as WFphase
+
+if nargin == 2 % Assume user isn't using an OptSys object, save input OS as WFamp, and WFamp as WFphase
     WFimag = WFreal;
     WFreal = OS;    
     numLambdas = size(WFreal,3);
@@ -40,6 +28,9 @@ elseif nargin == 3
         WFphase(:,:,jj) = atan2(WFimag(:,:,jj),WFreal(:,:,jj));
     end
     
+    %Store result in OS
+    OS.WFamp = WFamp;
+    OS.WFphase = WFphase;
 end
 
 end % of WFReIm2AmpPhase2
