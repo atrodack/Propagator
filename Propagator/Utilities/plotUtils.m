9560eee638556ee9c19ebcd9fb2c1073afa41af4
atrodack <atrodack@email.arizona.edu>
2017-06-02 09:55:16 -0700

    Updates to allow latex interpreter

--- Propagator/Utilities/plotUtils.m
+++ Propagator/Utilities/plotUtils.m
@@ -9,17 +9,48 @@ if nargin == 0
     colorbar;
     
 elseif nargin == 1
-    title(titl);
+    if length(titl) == 1
+        title(titl);
+    elseif length(titl) == 3
+        title(titl(1),titl(2),title(3));
+    end
     axis xy;
     axis square;
     colormap(gray(256));
     colorbar;
     
 elseif nargin == 3
-    title(titl);
-    xlabel(xlab);
-    ylabel(ylab);
-    axis xy;
+    if iscell(titl)
+        if length(titl) == 1
+            title(titl);
+        elseif length(titl) == 3
+            title(titl{1},titl{2},titl{3});
+        end
+    else
+        title(titl)
+    end
+    
+    if iscell(xlab)
+        if length(xlab) == 1
+            xlabel(xlab);
+        elseif length(xlab) == 3
+            xlabel(xlab{1},xlab{2},xlab{3});
+        end
+    else
+        xlabel(xlab)
+    end
+    
+   if iscell(ylab)
+        if length(ylab) == 1
+            ylabel(ylab);
+        elseif length(ylab) == 3
+            ylabel(ylab{1},ylab{2},ylab{3});
+        end
+    else
+        ylabel(ylab)
+   end
+   
+   axis xy;
     axis square;
     colormap(gray(256));
     colorbar;
